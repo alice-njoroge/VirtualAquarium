e03d18eec4d3655a5850c3afb3103f8900acc94c
Alice Wanjiku <alicenjoroge707@gmail.com>
2024-06-10 15:36:19 +0200

    Added a Fish to the aquarium

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,48 @@
 <script setup>
-
 import FishForm from "@/FishForm.vue";
+import {ref} from "vue";
+
+const show = ref();
+
 </script>
 <template>
-  <div class="view-port center aquarium-image">
+  <div class="viewport-center aquarium-image">
+    <div class="center-fish">
+      <img class="h-20" src="/golden-purple-fish.png" alt="" :style="{display: 'block'}">
+    </div>
     <FishForm/>
   </div>
 </template>
 
 <style>
+.viewport-center {
+  @apply flex items-center justify-center h-screen p-4;
+}
+
 .aquarium-image {
   background-image: url("/bg.jpg");
   background-attachment: fixed;
   margin: auto;
   height: 100vh
+}
+
+.box {
+  margin: 30px;
+  height: 50px;
+  width: 100px;
+  background-color: blue;
+}
+
+.box.horizTranslate {
+  -webkit-transition: 10s;
+  -moz-transition: 10s;
+  -ms-transition: 10s;
+  -o-transition: 10s;
+  transition: 10s;
+  margin-left: 50% !important;
+}
+
+.center-fish {
+  @apply flex flex-wrap gap-10 justify-center
 }
 </style>
--- src/FishForm.vue
+++ src/FishForm.vue
@@ -1,31 +1,54 @@
-<script setup></script>
+<script setup>
+import {ref} from "vue";
+const fishSelected = ref('');
+
+const types = ref(
+    [
+      {value: 'gold', url: '/goldfish.png'},
+      {value: 'golden', url: '/golden-purple-fish.png'},
+      {value: 'guppie', url: '/guppie.png'},
+      {value: 'tropical', url: '/tropical-fish.png'},
+      {value: 'tuna', url: '/tuna.png'},
+    ]);
+
+const handleSelection = (image) => {
+  fishSelected.value = image.value;
+  console.log("selected", fishSelected.value)
+}
+
+</script>
 <template>
   <div class="fish-form-wrapper bg-blue-700 h-100vh p-2">
     <div>
       <h2 class="text-white font-bold m-2"> Fish Type </h2>
     </div>
     <div class="h-80 m-5 flex flex-wrap justify-between">
-      <img class="h-20" src="/goldfish.png" alt="gold">
-      <img class="h-20" src="/golden-purple-fish.png" alt="gold">
-      <img class="h-20" src="/guppie.png" alt="gold">
-      <img class="h-20" src="/tropical-fish.png" alt="gold">
-      <img class="h-20" src="/tuna.png" alt="gold">
+      <img v-for="image in types"
+           class="h-20"
+           :src="image.url"
+           alt="gold"
+           @click="handleSelection(image)"
+      >
     </div>
     <div class="mb-4">
       <label class="block text-white font-bold mb-2" for="username">
         Name
       </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Mr.Buttons">
+      <input
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="username" type="text" placeholder="Mr.Buttons">
     </div>
     <div>
-      <button class="bg-red-700 hover:bg-red-800 text-white w-full font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+      <button
+          class="bg-red-700 hover:bg-red-800 text-white w-full font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="button">
         Add Fish
       </button>
     </div>
   </div>
 </template>
 <style scoped>
-.fish-form-wrapper{
+.fish-form-wrapper {
   position: absolute;
   width: 300px;
   top: 0;
