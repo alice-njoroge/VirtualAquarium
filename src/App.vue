da4bd0ff1215a8410c673cd6df3a695e2a22aee8
Alice Wanjiku <alicenjoroge707@gmail.com>
2024-06-10 13:35:07 +0200

    set the aquarium backround with css

--- src/App.vue
+++ src/App.vue
@@ -1,2 +1,17 @@
-<script setup></script>
-<template></template>
+<script setup>
+
+</script>
+<template>
+  <div class="view-port center aquarium-image">
+
+  </div>
+</template>
+
+<style>
+.aquarium-image {
+  background-image: url("/bg.jpg");
+  background-attachment: fixed;
+  margin: auto;
+  height: 100vh
+}
+</style>
